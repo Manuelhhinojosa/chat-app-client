5d038687437aff57a115f4a59a8b3fa6f2af7d54
Manuelhhinojosa <mangud.hinojosa@gmail.com>
2024-04-11 16:13:18 +0700

    Imported font from google fonts into app.css Imported chackra functionality into homePage component Created signup and login components (empty at the minute) inside their own dir, which is stored inside general components dir. Imported such new components into homePage component and used them inside the tab panel container

--- src/components/pageComponents/HomePage/HomePage.jsx
+++ src/components/pageComponents/HomePage/HomePage.jsx
@@ -1,7 +1,64 @@
+//default
 import React from "react";
 
+//dependencies
+import {
+  Container,
+  Box,
+  Text,
+  Tab,
+  Tabs,
+  TabList,
+  TabPanels,
+  TabPanel,
+} from "@chakra-ui/react";
+
+// components
+import Login from "../../generalComponents/loginComp/login";
+import Signup from "../../generalComponents/signupComp/signup";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <Container maxWidth="xl" centerContent>
+      <Box
+        d="flex"
+        p={3}
+        w="100%"
+        m="40px 0 15px 0"
+        textAlign={"center"}
+        border={"2px solid black"}
+        borderRadius={"1rem"}
+      >
+        <Text fontSize={"xx-large"} fontWeight={"300"}>
+          ChatApp
+        </Text>
+      </Box>
+
+      <Box w={"100%"} border={"2px solid black"} borderRadius={"1rem"}>
+        <Tabs variant="soft-rounded" colorScheme="orange" mt={"1rem"}>
+          <TabList d={"flex"} justifyContent={"space-around"}>
+            <Tab border={"1px solid black"} w={"30%"}>
+              Login
+            </Tab>
+
+            <Tab border={"1px solid black"} w={"30%"}>
+              Sign up
+            </Tab>
+          </TabList>
+
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
 };
 
 export default HomePage;
