76aae2c497b777d3ebec8c84951c5f4a8b6eefcb
Manuelhhinojosa <mangud.hinojosa@gmail.com>
2024-04-12 11:18:27 +0700

    Removed script that prevented css and .map sass files to autogenerate, added styles to chat component, index.jsx and app.jsc using SASS instead of CSS. Added variables to sass config scss file that imported and used in the rest of scss files.

--- src/components/pageComponents/HomePage/HomePage.jsx
+++ src/components/pageComponents/HomePage/HomePage.jsx
@@ -22,32 +22,21 @@ import Signup from "../../generalComponents/signupComp/signup";
 
 const HomePage = () => {
   return (
-    <Container maxWidth="xl" centerContent>
-      <Box
-        d="flex"
-        p={3}
-        w="100%"
-        m="40px 0 15px 0"
-        textAlign={"center"}
-        border={"2px solid black"}
-        borderRadius={"1rem"}
-      >
+    <Container className="container">
+      <Box className="boxTop">
         <Text fontSize={"xx-large"} fontWeight={"300"}>
           ChatApp
         </Text>
       </Box>
 
-      <Box w={"100%"} border={"2px solid black"} borderRadius={"1rem"}>
-        <Tabs variant="soft-rounded" colorScheme="orange" mt={"1rem"}>
-          <TabList d={"flex"} justifyContent={"space-around"}>
-            <Tab border={"1px solid black"} w={"30%"}>
-              Login
-            </Tab>
-
-            <Tab border={"1px solid black"} w={"30%"}>
-              Sign up
-            </Tab>
+      <Box className="boxBottom">
+        <Tabs className="tabs" variant="soft-rounded" colorScheme="orange">
+          <TabList className="tabList" justifyContent={"space-around"}>
+            <Tab className="tab">Login</Tab>
+            <Tab className="tab">Sign up</Tab>
           </TabList>
+
+          {/* here */}
 
           <TabPanels>
             <TabPanel>
